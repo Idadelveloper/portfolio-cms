a42ef7e1940d50dc60d6f6c30045c293fe573b01
Ida Delphine <idadelm@gmail.com>
2022-06-22 16:19:37 +0100

    Established mongodb connection

--- routes/articles.js
+++ routes/articles.js
@@ -5,4 +5,8 @@ router.get('/new', (req, res) => {
     res.render('articles/new')
 })
 
+router.post('/', (req, res) => {
+
+})
+
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const articleRouter = require('./routes/articles.js')
+const dotenv = require("dotenv")
+
 const app = express()
+dotenv.config()
+
+// connect to mongodb
+const MONGODB_URI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@portfoliodb.n5g0z.mongodb.net/?retryWrites=true&w=majority`;
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
 
 app.set('view engine', 'ejs')
 
