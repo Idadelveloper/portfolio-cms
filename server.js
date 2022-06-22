ebcb264abf1ca7cfd4664f6495822a7856b85f49
Ida Delphine <idadelm@gmail.com>
2022-06-22 15:10:41 +0100

    Created articles page

--- server.js
+++ server.js
@@ -8,7 +8,20 @@ app.use('/articles', articleRouter)
 
 
 app.get('/', (req, res) => {
-    res.render('index')
+    const articles = [{
+            title: 'Test article',
+            createdAt: new Date(),
+            description: 'Test description'
+        },
+        {
+            title: 'Test article 2',
+            createdAt: new Date(),
+            description: 'Test description 2'
+        },
+
+    ]
+
+    res.render('index', { articles: articles })
 })
 
 app.listen(5000, () => {
