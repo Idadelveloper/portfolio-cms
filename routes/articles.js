f701beebadc508a06dc88e3eb30c4d7c24a46a01
Ida Delphine <idadelm@gmail.com>
2022-06-22 21:54:50 +0100

    Created delete route

--- routes/articles.js
+++ routes/articles.js
@@ -33,4 +33,9 @@ router.post('/', async(req, res) => {
 
 })
 
+router.delete('/:id', async(req, res) => {
+    await Article.findByIdAndDelete(req.params.id)
+    res.redirect('/')
+})
+
 module.exports = router;
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const Article = require('./models/article')
 const articleRouter = require('./routes/articles.js')
 const dotenv = require("dotenv")
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override')
 
 const app = express()
 dotenv.config()
@@ -12,6 +13,7 @@ app.set('view engine', 'ejs')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.use(methodOverride('_method'))
 app.use('/articles', articleRouter)
 
 // connect to mongodb
